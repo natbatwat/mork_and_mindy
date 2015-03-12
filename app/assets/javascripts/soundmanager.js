085471fbd55de66a3ea7576e4c3c22160488bfd5
Colin Durbin <cddurbin@gmail.com>
2015-03-12 18:34:30 +0000

    Attempt to get soundmanager working.

---
+++ app/assets/javascripts/soundmanager.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  soundManager.setup({
+    url: 'js/soundmanager2/swf',
+    onready: function() {
+      console.log('hi');
+      // var hello = soundManager.createSound({
+      //   id: 'test',
+      //   url: 'audio/break8.wav'  
+      // });
+      // console.log(hello);    
+    },
+    ontimeout: function() {
+      // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
+    }
+  });
+  soundManager.onready(function(){
+    var hello = soundManager.createSound({
+      id: 'test',
+      url: 'audio/break8.wav'  
+    });
+
+  });
+ 
+
+  $(document).on("keypress", function(e){
+    actualkey=String.fromCharCode(e.keyCode)
+    if (actualkey === 'a') {
+      console.log("pressed!");
+      // hello.play();
+      soundManager.play('test');
+    } else {
+      console.log('fail');
+    }
+  });
+});
