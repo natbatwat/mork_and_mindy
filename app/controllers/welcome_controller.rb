1944e5f9f974758cc432b66b38c826c86eb14e8f
Natalie Loh <Natalie@Natalies-MacBook-Pro-2.local>
2015-03-12 16:02:27 +0000

    finished sg feed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @singapore_photos = Instagram.tag_recent_media('singapore')
+    @singapore_photos = Instagram.tag_recent_media('singaporelandscape')
   end
 end
