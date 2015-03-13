42dc2656a99f04054475f25c7a84fa0050c541b7
Colin Durbin <cddurbin@gmail.com>
2015-03-13 06:39:01 +0000

    Built coordinates grabber.

--- app/assets/javascripts/london.js
+++ app/assets/javascripts/london.js
@@ -1,35 +1,23 @@
 $(document).ready(function(){
   var tardis_lat = 51.492137
   var tardos_long = -0.192878
-  // to be cut/pasted at end of challenge before
-  $('h1').on("click", function(){
-    function initialize() {
-      var earlsCourt = new google.maps.LatLng(51.492137,-0.192878);
-      var mapOptions = {
-        center: earlsCourt,
-        zoom: 14
-      };
-      var map = new google.maps.Map(
-          document.getElementById('map-canvas'), mapOptions);
-      var panoramaOptions = {
-        position: earlsCourt,
-        pov: {
-          heading: 34,
-          pitch: 10
-        }
-      };
-      var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
-      map.setStreetView(panorama);
-      }
-    initialize();
-  });
-  // north
-  // 51.492235,-0.193035
-  // 51.492333,-0.193195
-  // 51.492449,-0.193401
-  // south
-  // 51.492108,-0.192822
-  // 51.492036,-0.192702
+
+  function getLong(){
+    var mapUrl = $('a').attr('href');
+    var newMapUrl = mapUrl.split('=');
+    var nextMapUrl = newMapUrl.shift();
+    var coordinates = newMapUrl.shift();
+    var latLong = coordinates.split(',')[1];
+    var longitude = latLong.substr(5);
+    var long = longitude.substr(0, longitude.length-2);
+    temps(long);
+  }
+
+  // console.log(coordinates);
+  // console.log(latLong);
+  // console.log(longitude);
+  // console.log(long);
+ 
 
   function temps(long){
     if (long >= 2850 && long <= 2900) {
@@ -46,9 +34,32 @@ $(document).ready(function(){
       console.log('out of bounds');
     }
   }
-  
-  
-  
+
+  $('h2').on("click", function (){
+    getLong();
+  });
 
 
+  // to be cut/pasted at end of challenge before
+  $('h1').on("click", function(){
+    function initialize() {
+      var earlsCourt = new google.maps.LatLng(51.4924328,-0.1933711);
+      var mapOptions = {
+        center: earlsCourt,
+        zoom: 18
+      };
+      var map = new google.maps.Map(
+          document.getElementById('map-canvas'), mapOptions);
+      var panoramaOptions = {
+        position: earlsCourt,
+        pov: {
+          heading: 34,
+          pitch: 10
+        }
+      };
+      var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+      map.setStreetView(panorama);
+      }
+    initialize();
+  });
 });
