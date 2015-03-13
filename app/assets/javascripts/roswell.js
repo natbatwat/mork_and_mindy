4b866a3c5ce5fdf32490bf5c3e7af914382524c1
Natalie Loh <Natalie@Natalies-MacBook-Pro-2.local>
2015-03-13 11:38:02 +0000

    finished roswell

--- app/assets/javascripts/roswell.js
+++ app/assets/javascripts/roswell.js
@@ -1,24 +1,42 @@
 $(document).ready(function(){
-  // $('h1').on("click", function(){
-  //   $.ajax({
-  //     url: 'http://api.sba.gov/geodata/city_county_links_for_state_of/nm.json',
-  //     type: 'GET',
-  //     dataType: 'json'
-  //   })
-  //   .done(function(data){
-  //     console.log(data);
-  //   });
-  // });
-  // $('h1').on("click", function(){
-  //   function initialize() {
-  //     console.log('new map');
-  //     var mapOptions = {
-  //       zoom: 10,
-  //       center: new google.maps.LatLng(104.5281, 33.3872)
-  //     };
-  //     map = new google.maps.Map(document.getElementById('map-canvas'),
-  //         mapOptions);
-  //   }
-  //   initialize();
-  // });
+  var next4 = $('#ros-next');
+  var input = $('#input-box');
+  
+  next4.click(function(){
+    $('.mindy-text-4').children('.active').addClass('done').removeClass('active');
+    $('.mindy-text-4').children('.hide').first().removeClass('hide').addClass('active');
+  })
+
+  function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, min = i - size, temp, index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
+  }
+
+  var movies = ['alien', 'et', 'guardiansofthegalaxy', 'starwars'];
+  var randomMovie = getRandomSubarray(movies, 1)[0];
+  var randomMovieToUnhide = $('#' + randomMovie);
+  console.log('MOVIE SELECTED' + randomMovie);
+  randomMovieToUnhide.removeClass('hide');
+
+  $('#ros-next').click(function(){
+    if (input.val().toLowerCase().replace(/\s/g,'') === randomMovie) {
+      function initialize() {
+        console.log('movie map');
+        var mapOptions = {
+          zoom: 7,
+          center: new google.maps.LatLng(33.968333, -105.243333)
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      initialize();
+    };
+  })
+
 });
