8b26a92fb080f499cec44d0ef725223d15f68dab
Natalie Loh <Natalie@natalies-mbp-2.home>
2015-04-09 14:36:03 +0100

    commit

--- app/assets/javascripts/singapore.js
+++ app/assets/javascripts/singapore.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   console.log('ready');
   var styles = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#193341"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#2c5a71"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#29768a"},{"lightness":-37}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#406d80"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#406d80"}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#3e606f"},{"weight":2},{"gamma":0.84}]},{"elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"weight":0.6},{"color":"#1a3541"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#2c5a71"}]}];
   
-  var input = $('#input-box')
+  var input = $('#input-box');
   var next1 = $('#sg-next-1');
   next1.click(function(){
     console.log('next1 clicked');
